412e52aa82ea8bbdb802fcfea7d0e0b1ba9e461f
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-02-11 17:49:17 -0700

    Add files via upload

---
+++ Scripts/MouseToWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseToWorld : MonoBehaviour
+{
+    public GameObject flowerRef;
+    private void Update()
+    {
+        Vector3 pos = Input.mousePosition;
+        pos.z = 60.5f;
+        pos = Camera.main.ScreenToWorldPoint(pos);
+        transform.position = pos;
+        transform.position = new 
+            Vector3(transform.position.x, flowerRef.transform.position.y, transform.position.z);
+    }
+}
